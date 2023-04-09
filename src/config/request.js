0e3553910216a38baaab60fe91b228525d56a840
SUNIL PATIL <sunilpatil12000@gmail.com>
2023-04-09 21:34:57 +0530

    t4

--- src/config/request.js
+++ src/config/request.js
@@ -1,25 +1,17 @@
-
-
-
-
-function request() {
-
-
-    const API_KEY ='4208d8f49955b6ffd12ca89155f07ba4'
-
 const request = {
-    fatchNetflixShow:'/discover/tv?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_networks=213',
-    fatchTranding:'/trending/all/day?api_key=4208d8f49955b6ffd12ca89155f07ba4',
-    fetchTopRated:'/movie/top_rated?api_key=4208d8f49955b6ffd12ca89155f07ba4&language=en-US',
-    fetchHorror:'/discover/movie?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_genres=27',
-    fetchComedy:'/discover/movie?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_genres=35',
-    fetchRomance:'/discover/movie?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_genres=10749',
-    fetchScifi:'/discover/movie?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_genres=878',
-}
-
-
-  
-}
+  fatchNetflixShow:
+    "/discover/tv?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_networks=213",
+  fatchTranding: "/trending/all/day?api_key=4208d8f49955b6ffd12ca89155f07ba4",
+  fetchTopRated:
+    "/movie/top_rated?api_key=4208d8f49955b6ffd12ca89155f07ba4&language=en-US",
+  fetchHorror:
+    "/discover/movie?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_genres=27",
+  fetchComedy:
+    "/discover/movie?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_genres=35",
+  fetchRomance:
+    "/discover/movie?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_genres=10749",
+  fetchScifi:
+    "/discover/movie?api_key=4208d8f49955b6ffd12ca89155f07ba4&with_genres=878",
+};
 
 export default request;
-
